25b0f84ca319968193672cbb576cefe55524fb72
Makaroshka1234 <makstutik007@gmail.com>
2025-03-24 14:18:43 +0200

    add bg cardside and click logic

--- src/components/CartSide/CartSide.jsx
+++ src/components/CartSide/CartSide.jsx
@@ -11,15 +11,31 @@ const CartSide = ({ activeCart, setActiveCart }) => {
     const { itemCount } = useSelector(state => state.cart)
     const { cartItems, totalPrice } = useSelector(state => state.cart)
 
+    const cardRef = useRef()
 
+        ;
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (cardRef.current && !cardRef.current.contains(event.target)) {
+                setActiveCart(false);
+            }
+        };
+
+        if (activeCart) {
+            document.addEventListener("mousedown", handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [activeCart]);
 
 
 
     function removeActiveCart() {
         setActiveCart(false)
     }
-
-
 
     const listRef = useRef()
 
@@ -31,52 +47,53 @@ const CartSide = ({ activeCart, setActiveCart }) => {
 
 
     return (
-
-        <div className={`cardside ${activeCart ? 'active' : ''}`} >
-            <div className="cardside__inner">
-                <div className="cardside-top">
-                    <h1 className="top-title">Корзина</h1>
-                    <img src="/images/close.svg" alt="" className='cart__close-btn' onClick={removeActiveCart} />
-                </div>
-                <ul ref={listRef} className='cardside__list'>
-                    {cartItems.length <= 0 ? 'Ваша корзина порожня' : cartItems.map(item => (
-                        <li key={item.id} className="cartside__list-item">
-                            <img src={item.imgUrl} alt="item" className='item-img' />
-                            <p className="item-title">{item.title}</p>
-                            <div className="item-counter">
-                                <button className="plus-btn" onClick={() => dispatch(addCount(item))}  >+</button>
-                                <input value={item.count} type="text" />
-                                <button className='minus-btn' onClick={() => dispatch(removeCount(item))} >-</button>
-                            </div>
-                            <p className="item-price">{Number(item.price) * item.count}грн </p>
-                            <button
-
-                                className="item-delete-btn"
-                                onClick={() => dispatch(removeItem(item.id))}
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 128 128"
-                                    className="w-6 h-6"
-                                    fill="currentColor"
-                                >
-                                    <path d="M64,0a64,64,0,1,0,64,64A64.07,64.07,0,0,0,64,0Zm0,122a58,58,0,1,1,58-58A58.07,58.07,0,0,1,64,122Z" />
-                                    <path d="M92.12,35.79a3,3,0,0,0-4.24,0L64,59.75l-23.87-24A3,3,0,0,0,35.88,40L59.76,64,35.88,88a3,3,0,0,0,4.25,4.24L64,68.25l23.88,24A3,3,0,0,0,92.13,88L68.24,64,92.13,40A3,3,0,0,0,92.12,35.79Z" />
-                                </svg>
-                            </button>
-                        </li>
-                    ))}
-                </ul>
-                <div className="cardside__bottom">
-                    <div className="cardide__bottom-section">
-                        <p className='cardside__price-text'>До оплати: </p>
-                        <h1 className='cardside-total'>{totalPrice} грн</h1>
+        <div className={`cardside-wrapper ${activeCart ? 'active' : ''}`}>
+            <div ref={cardRef} className={`cardside ${activeCart ? 'active' : ''}`} >
+                <div className="cardside__inner">
+                    <div className="cardside-top">
+                        <h1 className="top-title">Корзина</h1>
+                        <img src="/images/close.svg" alt="" className='cart__close-btn' onClick={removeActiveCart} />
                     </div>
+                    <ul ref={listRef} className='cardside__list'>
+                        {cartItems.length <= 0 ? 'Ваша корзина порожня' : cartItems.map(item => (
+                            <li key={item.id} className="cartside__list-item">
+                                <img src={item.imgUrl} alt="item" className='item-img' />
+                                <p className="item-title">{item.title}</p>
+                                <div className="item-counter">
+                                    <button className="plus-btn" onClick={() => dispatch(addCount(item))}  >+</button>
+                                    <input value={item.count} type="text" />
+                                    <button className='minus-btn' onClick={() => dispatch(removeCount(item))} >-</button>
+                                </div>
+                                <p className="item-price">{Number(item.price) * item.count}грн </p>
+                                <button
 
-                    <button className='cardside_btn'>Оформити замовлення</button>
+                                    className="item-delete-btn"
+                                    onClick={() => dispatch(removeItem(item.id))}
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 128 128"
+                                        className="w-6 h-6"
+                                        fill="currentColor"
+                                    >
+                                        <path d="M64,0a64,64,0,1,0,64,64A64.07,64.07,0,0,0,64,0Zm0,122a58,58,0,1,1,58-58A58.07,58.07,0,0,1,64,122Z" />
+                                        <path d="M92.12,35.79a3,3,0,0,0-4.24,0L64,59.75l-23.87-24A3,3,0,0,0,35.88,40L59.76,64,35.88,88a3,3,0,0,0,4.25,4.24L64,68.25l23.88,24A3,3,0,0,0,92.13,88L68.24,64,92.13,40A3,3,0,0,0,92.12,35.79Z" />
+                                    </svg>
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                    <div className="cardside__bottom">
+                        <div className="cardide__bottom-section">
+                            <p className='cardside__price-text'>До оплати: </p>
+                            <h1 className='cardside-total'>{totalPrice} грн</h1>
+                        </div>
+
+                        <button className='cardside_btn'>Оформити замовлення</button>
+                    </div>
                 </div>
-            </div>
 
+            </div>
         </div>
 
     )
